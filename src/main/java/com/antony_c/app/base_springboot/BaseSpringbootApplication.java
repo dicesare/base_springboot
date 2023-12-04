024fd743b12f168ca58020040908a77feb35faa5
dicesare <antony.coco@live.fr>
2023-12-04 12:14:52 +0100

    first commit

---
+++ src/main/java/com/antony_c/app/base_springboot/BaseSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.antony_c.app.base_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaseSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/antony_c/app/base_springboot/BaseSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.antony_c.app.base_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
